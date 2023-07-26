5cd0c5f3192bca24b0e7230a83f335719be1d855
aryark <aryark3112@gmail.com>
2023-07-26 09:26:54 +0530

    first commit

---
+++ Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExample1
+{
+    //[A.S.] [Modifier] class <ClassName>:[Base ClassName][Interface]
+    //{
+    //Members of the class
+    //fields
+    //properties
+    //constructor
+    //methods
+    //indexer
+    //events
+    //delegates
+    //}
+    public class Employee
+    {
+        int id;
+        string name;
+        public Employee()
+        {
+            Console.WriteLine("Employee Constructor");
+            id = -1;
+            name = "not given";
+        }
+        public void Register()
+        {
+            Console.WriteLine("Enter Id");
+            id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Name");
+            name = Console.ReadLine();
+        }
+        public void Display()
+        {
+            Console.WriteLine("ID: "+id);
+            Console.WriteLine("Name: " + name);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OOPExample1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ClassName objectName= new ClassName();
+            Employee employee = new Employee();
+            Employee employee2 = new Employee();
+            Console.WriteLine("Enter Employee One Details");
+            employee.Register();
+            Console.WriteLine("Enter Employee Two Details");
+            employee2.Register();
+            Console.WriteLine("Employee One Display");
+            employee.Display();
+            Console.WriteLine("Employee Two Display");
+            employee2.Display();
+            Console.ReadKey();
+        }
+    }
+}
